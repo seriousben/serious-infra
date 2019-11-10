0c4c1d8b745779e3b7a38535773406819c476026
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2019-11-09 21:10:42 -0500

    add IAM for seriousben.com CD

--- iam.tf
+++ iam.tf
@@ -1,9 +1,20 @@
 resource "google_service_account" "terraform-viewer" {
   account_id   = "terraform-viewer"
-  display_name = "Terraform viewer for Continuous Integration"
+  display_name = "https://github.com/seriousben/serious-infra CI"
 }
 
 resource "google_project_iam_member" "terraform-all-viewer" {
   role   = "roles/viewer"
   member = "serviceAccount:${google_service_account.terraform-viewer.email}"
 }
+
+resource "google_service_account" "seriousben-com-deployer" {
+  account_id   = "serousben-com-deployer"
+  display_name = "https://github.com/seriousben/seriousben.com CD"
+}
+
+resource "google_project_iam_member" "serousben-com-deployer" {
+  role   = "roles/container.developer"
+  member = "serviceAccount:${google_service_account.seriousben-com-deployer.email}"
+}
+
