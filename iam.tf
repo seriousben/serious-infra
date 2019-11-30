a89248a83202c0ae72bf28ff6fadf6afe10fbc44
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2019-11-30 13:15:32 -0500

    add serious-watcher backend sa

--- iam.tf
+++ iam.tf
@@ -1,3 +1,7 @@
+#####
+# serious-infra
+#####
+
 resource "google_service_account" "terraform-viewer" {
   account_id   = "terraform-viewer"
   display_name = "https://github.com/seriousben/serious-infra CI"
@@ -7,6 +11,10 @@ resource "google_project_iam_member" "terraform-all-viewer" {
   role   = "roles/viewer"
   member = "serviceAccount:${google_service_account.terraform-viewer.email}"
 }
+
+#####
+# seriousben.com
+#####
 
 resource "google_service_account" "seriousben-com-deployer" {
   account_id   = "seriousben-com-deployer"
@@ -18,6 +26,10 @@ resource "google_project_iam_member" "seriousben-com-deployer" {
   member = "serviceAccount:${google_service_account.seriousben-com-deployer.email}"
 }
 
+#####
+# Newsblur to hugo
+#####
+
 resource "google_service_account" "newsblur-to-hugo-deployer" {
   account_id   = "newsblur-to-hugo-deployer"
   display_name = "https://github.com/seriousben/newsblur-to-hugo CD"
@@ -28,6 +40,10 @@ resource "google_project_iam_member" "newsblur-to-hugo-deployer" {
   member = "serviceAccount:${google_service_account.newsblur-to-hugo-deployer.email}"
 }
 
+#####
+# Serious Watcher
+#####
+
 resource "google_service_account" "serious-watcher-deployer" {
   account_id   = "serious-watcher-deployer"
   display_name = "https://github.com/seriousben/serious-watcher CD"
@@ -36,5 +52,15 @@ resource "google_service_account" "serious-watcher-deployer" {
 resource "google_project_iam_member" "serious-watcher-deployer" {
   role   = "roles/container.viewer"
   member = "serviceAccount:${google_service_account.serious-watcher-deployer.email}"
+}
+
+resource "google_service_account" "serious-watcher-backend" {
+  account_id   = "serious-watcher-backend"
+  display_name = "https://github.com/seriousben/serious-watcher backend"
+}
+
+resource "google_project_iam_member" "serious-watcher-backend" {
+  role   = "roles/datastore.user"
+  member = "serviceAccount:${google_service_account.serious-watcher-backend.email}"
 }
 
